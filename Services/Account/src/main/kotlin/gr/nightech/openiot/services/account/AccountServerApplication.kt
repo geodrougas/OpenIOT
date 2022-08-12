83164719a74da09c865e104474ebaa007d0889a3
geodrougas <georgios.drougas@gmail.com>
2022-08-12 18:20:38 +0300

    Reorganization

--- Services/Account/src/main/kotlin/gr/nightech/openiot/services/account/AccountServerApplication.kt
+++ Services/Account/src/main/kotlin/gr/nightech/openiot/services/account/AccountServerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AuthenticationServerApplication
+class AccountServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<AuthenticationServerApplication>(*args)
+	runApplication<AccountServerApplication>(*args)
 }
